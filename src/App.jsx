d107671aca90edf94b2bcb815f5097a6051d6df7
Sebastian Safari <ssafari@parc.com>
2020-06-29 11:04:56 -0700

    basic tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import TabMgr from './components/TabMgr'
 
 const App = () => {
     return (
         <div>
             <Header/>
             <h1>Multi-Tasks</h1>
+            <TabMgr/>
             <Footer />
         </div>
     )
---
+++ src/components/TabMgr.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Tabs, Tab } from 'react-bootstrap'
+
+const TabMgr = () => {
+    const [tabKey, setTabKey] = useState("home")
+    return (
+        <div>
+            <Tabs activeKey={tabKey} id="tabs" onSelect={k => setTabKey(k)}>
+                <Tab eventKey="home" title="home">
+                    <p>Home page</p>
+                </Tab>
+                <Tab eventKey="user1" title="user1">
+                    <p>User 1</p>
+                </Tab>
+            </Tabs>
+        </div>
+    )
+}
+
+export default TabMgr
