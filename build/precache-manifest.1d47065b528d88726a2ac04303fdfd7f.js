03527ad3e1b94c3bb74b9bb8c467920ef4cfc3e7
Sebastian Safari <ssebs@ymail.com>
2020-07-06 14:27:51 -0700

    Added build to github so I can copy to PI without building

---
+++ build/precache-manifest.1d47065b528d88726a2ac04303fdfd7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d8714413baebda2b3acb636adc8540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9475a4dcf062426ce026",
+    "url": "/static/css/2.e42ee28c.chunk.css"
+  },
+  {
+    "revision": "3ca81a1ee7064a4bf810",
+    "url": "/static/css/main.83e0a8d4.chunk.css"
+  },
+  {
+    "revision": "9475a4dcf062426ce026",
+    "url": "/static/js/2.40f013b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.40f013b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca81a1ee7064a4bf810",
+    "url": "/static/js/main.a0e1db3d.chunk.js"
+  },
+  {
+    "revision": "d2d8f75150af991202a8",
+    "url": "/static/js/runtime-main.493458e9.js"
+  }
+]);
