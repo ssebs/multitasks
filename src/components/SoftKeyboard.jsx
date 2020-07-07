84f29440d8a66957b8bf306699fc408cc945e868
Sebastian Safari <ssebs@ymail.com>
2020-07-06 19:28:05 -0700

    convert to dark mode

--- src/components/SoftKeyboard.jsx
+++ src/components/SoftKeyboard.jsx
@@ -3,7 +3,7 @@ import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 
 const SoftKeyboard = (props) => {
-    return <Keyboard onChange={props.onChange} onKeyPress={props.onKeyPress} />;
+    return <Keyboard onChange={props.onChange} onKeyPress={props.onKeyPress} theme={"hg-theme-default myTheme1"} />;
 };
 
 export default SoftKeyboard;
